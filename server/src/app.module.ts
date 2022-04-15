288817ad9aaa980cf5f29ac2fb7d1d5b0decf2bd
jonathanPretre <jonathan.pretre91@gmail.com>
2022-04-15 10:41:13 +0900

    settings

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,31 @@
 import { Module } from '@nestjs/common';
-// import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 import { LoggerModule } from './infrastructure/logger/logger.module';
 import { ExceptionsModule } from './infrastructure/exceptions/exceptions.module';
 import { UsecasesProxyModule } from './infrastructure/usecases-proxy/usecases-proxy.module';
 import { ControllersModule } from './infrastructure/controllers/controllers.module';
+import { BcryptModule } from './infrastructure/services/bcrypt/bcrypt.module';
+import { JwtModule as JwtServiceModule } from './infrastructure/services/jwt/jwt.module';
+import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module';
+import { LocalStrategy } from './infrastructure/common/strategies/local.strategy';
+import { JwtStrategy } from './infrastructure/common/strategies/jwt.strategy';
+import { JwtRefreshTokenStrategy } from './infrastructure/common/strategies/jwtRefresh.strategy';
 
 @Module({
-  imports: [LoggerModule, ExceptionsModule, UsecasesProxyModule, ControllersModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.secret,
+    }),
+    LoggerModule,
+    ExceptionsModule,
+    UsecasesProxyModule.register(),
+    ControllersModule,
+    BcryptModule,
+    JwtServiceModule,
+    EnvironmentConfigModule,
+  ],
+  providers: [LocalStrategy, JwtStrategy, JwtRefreshTokenStrategy],
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './infrastructure/common/filter/exception.filter';
 import { LoggingInterceptor } from './infrastructure/common/interceptors/logger.interceptor';
@@ -10,6 +11,8 @@ import { LoggerService } from './infrastructure/logger/logger.service';
 async function bootstrap() {
   const env = process.env.NODE_ENV;
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
 
   // Filter
   app.useGlobalFilters(new AllExceptionFilter(new LoggerService()));
